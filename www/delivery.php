70388ef9de6bdb41e2cef4cd6e6f38673e24524b
Константинов Дмитрий <poethrenoff@gmail.com>
2013-02-24 10:01:01 +0400

    Доставка

---
+++ www/delivery.php
@@ -0,0 +1,11 @@
+<?
+	require 'templates/common.inc';
+
+	list($title, $keywords, $description) = get_head('delivery', 0);
+
+	require 'templates/header.inc';
+
+	require 'templates/delivery.inc';
+
+	require 'templates/footer.inc';
+?>
